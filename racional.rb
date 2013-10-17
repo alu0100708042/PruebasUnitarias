8c8d1aba9a6fae75ba8c4b165a476bcbb27f6989
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-10-17 21:54:27 +0100

    Método del constructor y mostrado del racional

--- racional.rb
+++ racional.rb
@@ -2,5 +2,14 @@
 
 require "./gcd.rb"
 
+	#incializacion del objeto, llamada al método del constructor
+	def initialize (a,b)
+		@x=a
+		@y=b	
+	end
+	#convertir a string para mostrarlo 	
+        def to_s
+                "#{@x}/#{@y}"
+        end
 class Fraccion
 end
