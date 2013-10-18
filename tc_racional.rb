5ee0ffcdb34e574b3d0b3bd5edae4eab70d75184
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-10-18 11:48:03 +0100

    inicializando las pruebas

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,8 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+   def test_suma
+      assert_equal([2, 1], Fraccion.new(1,1).suma(1,1)) # (1/1) + (1/1) = (2/1)
+      assert_equal([17, 10], Fraccion.new(3,2).suma(1,5)) # (3/2) + (1/5) = (17/10)
+   end
 end
