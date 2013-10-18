0ffe36fc6565ceb35d41ec8e3f5b701771474cab
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-10-18 17:09:08 +0100

    prueba final

--- tc_racional.rb
+++ tc_racional.rb
@@ -12,22 +12,22 @@ class Test_Fraccion < Test::Unit::TestCase
 	@d = Fraccion.new(1,5)
    end
    def test_suma
-      assert_equal([5, 3],@a.suma(@b))
-      assert_equal([6, 5],@a.suma(@d)) 
+      assert_equal("5/3",@a.suma(@b).to_s)
+      assert_equal("6/5",@a.suma(@d).to_s) 
    end
 
    def test_resta
-      assert_equal([1, 3],@a.resta(@b))
-      assert_equal([4, 5],@a.resta(@d))
+      assert_equal("1/3",@a.resta(@b).to_s)
+      assert_equal("4/5",@a.resta(@d).to_s)
    end
    
    def test_producto
-      assert_equal([2, 3],@a.producto(@b))
-      assert_equal([6, 15],@b.producto(@c))
+      assert_equal("2/3",@a.producto(@b).to_s)
+      assert_equal("6/15",@b.producto(@c).to_s)
    end
    
    def test_division
-      assert_equal([3, 2],@a.division(@b))
-      assert_equal([10, 9],@b.division(@c))
+      assert_equal("3/2",@a.division(@b).to_s)
+      assert_equal("15/6",@b.division(@c).to_s)
    end
 end
